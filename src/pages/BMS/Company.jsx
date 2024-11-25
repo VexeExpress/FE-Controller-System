c537257ab190c184d634b23bc3bc07e8f6828f08
minwang03 <93901911+minwang03@users.noreply.github.com>
2024-11-25 15:05:02 +0700

    u

--- src/pages/BMS/Company.jsx
+++ src/pages/BMS/Company.jsx
@@ -4,14 +4,14 @@ import { CompanyTable } from '../../components/company/CompanyTable';
 export function Company() {
   const [loading, setLoading] = useState(true);
   const [companies, setCompanies] = useState([]);
-  const [searchTerm, setSearchTerm] = useState(''); 
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
     const fetchCompanies = async () => {
       try {
         const response = await fetch('http://localhost:3000/api/company/getAllCompany');
         const result = await response.json();
-        setCompanies(result.data.companiesData);
+        setCompanies(result.data);
         setLoading(false);
       } catch (error) {
         setLoading(false);
@@ -35,7 +35,7 @@ export function Company() {
     } else {
       const response = await fetch('http://localhost:3000/api/company/getAllCompany');
       const result = await response.json();
-      setCompanies(result.data.companiesData);
+      setCompanies(result.data);
       setLoading(false);
     }
   };
